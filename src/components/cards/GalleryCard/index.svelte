5db652a6d175f833e1a572d9f8b75ccaa2614b2c
ruvkr <34903858+ruvkr@users.noreply.github.com>
2021-06-13 21:25:20 +0530

    Image gallery

---
+++ src/components/cards/GalleryCard/index.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  export let _id: string;
+  export let thumb: string;
+  export let path: string;
+  export let user_name: string;
+  export let user_url: string;
+  export let photo_url: string;
+</script>
+
+<div>
+  <img src={thumb} alt="Photo by {user_name}" />
+</div>
+
+<style lang="scss">
+  div {
+    width: 100%;
+  }
+  img {
+    display: block;
+    width: 100%;
+    height: auto;
+    margin: 0;
+  }
+</style>
--- src/pages/home/index.svelte
+++ src/pages/home/index.svelte
@@ -1,68 +1,11 @@
 <script lang="ts">
+  import Masonry from '../../components/Masonry/index.svelte';
+  import GalleryCard from '../../components/cards/GalleryCard/index.svelte';
   import { images } from './images';
-  import type { ImageData } from './images';
-  const columnWidth = 320;
-  let innerWidth = window.innerWidth;
-
-  // reactive values
-  $: columns = Math.floor(innerWidth / columnWidth);
-  $: imageColumns = images.reduce<ImageData[][]>((acc, cur, i) => {
-    const index = i % columns;
-    acc[index] = [...acc[index], cur];
-    return acc;
-  }, new Array(columns).fill([]));
-
-  console.log(imageColumns);
-  $: console.log(imageColumns);
 </script>
 
-<svelte:window bind:innerWidth />
-
-<main>
-  {#if imageColumns}
-    <div class="container" style="--columns:{columns}">
-      {#each imageColumns as column, i (i)}
-        <div class="column">
-          {#each column as image (image._id)}
-            <img alt={image._id} src={image.thumb} />
-          {/each}
-        </div>
-      {/each}
-    </div>
-  {:else}
-    <p>Working</p>
-  {/if}
-</main>
+<Masonry dataArray={images} component={GalleryCard} />
 
 <style lang="scss">
-  $columns: var(--columns);
-  $gap: 10px;
-
-  main {
-    width: 100%;
-    height: 100%;
-    overflow-x: hidden;
-    overflow-y: auto;
-    padding: $gap;
-    backface-visibility: hidden;
-  }
-
-  .container {
-    display: grid;
-    grid-template-columns: repeat($columns, 1fr);
-    grid-gap: $gap;
-  }
-
-  .column {
-    display: grid;
-    grid-gap: $gap;
-    grid-auto-rows: max-content;
-  }
-
-  img {
-    width: 100%;
-    height: auto;
-    display: block;
-    margin: 0;
-  }
+  //
 </style>
